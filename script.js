242cf8d62bb4d7f6fca5cf26407e54a5890253f6
Muhterem12 <alkanahmetmuho@gmail.com>
2024-10-23 18:46:36 +0300

    2

--- script.js
+++ script.js
@@ -3,9 +3,15 @@
 let inputNum = document.querySelector(".input-num");
 
 inputNum.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") pascalConverter();
+  if (e.key === "Enter") console.log(pascalConverter());
 });
 
-const pascalConverter = function () {
-  console.log("test");
+const pascalConverter = function (n) {
+  let total = [];
+
+  for (let row = 1; row <= n; ++) {
+    for (let column = 0; column < ; column++) {
+      // ...
+    }    
+  }
 };
